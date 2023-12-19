7714da8f00e522fbf8878b5b8ad02a56cbe773fe
Mhamudul_Islam <141499999+Mhamudulislam@users.noreply.github.com>
2023-12-19 20:58:10 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('.my_search').mouseenter(function()
+    {
+        $('#my_input').toggle();
+    });
+
+
+
+    // MixItUp
+var mixer = mixitup('#recent_ites_work', {
+    animation: {
+        duration: 300
+    }
+});
+});
